40fee2e11a273c7e3a43a40e2ad96b423d569cef
alfism1 <alfialfarisi@gmail.com>
2022-06-20 19:46:21 +0700

    update rabbit mq code

--- pzem_reader.py
+++ pzem_reader.py
@@ -138,23 +138,39 @@ if __name__ == "__main__":
     params = pika.URLParameters(url)
     params.heartbeat = 0
     params.blocked_connection_timeout = 0
-    connection = pika.BlockingConnection(params)
-    channel = connection.channel()  # start a channel
-    channel.queue_declare(queue=queue_name, durable=True)  # Declare a queue
+    while True:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()  # start a channel
+        channel.queue_declare(
+            queue=queue_name, durable=True)  # Declare a queue
 
-    def callback(ch, method, properties, body):
-        decode = body.decode("utf-8")
-        daya = json.loads(decode)["daya_kwh"]
-        splu_process("/dev/ttyUSB0", 23, daya)
-        print("Waiting for the next message")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        def callback(ch, method, properties, body):
+            decode = body.decode("utf-8")
+            daya = json.loads(decode)["daya_kwh"]
+            splu_process("/dev/ttyUSB0", 23, daya)
+            print("Waiting for the next message")
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    # set up subscription on the queue
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue_name,
-                          callback)
+        # set up subscription on the queue
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue_name,
+                              callback)
 
-    # start consuming (blocks)
-    print("Waiting for message")
-    channel.start_consuming()
-    connection.close()
+        try:
+            print('[*] Waiting for messages. To exit press CTRL+C')
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+            connection.close()
+        except pika.exceptions.ConnectionClosedByBroker:
+            # Uncomment this to make the example not attempt recovery
+            # from server-initiated connection closure, including
+            # when the node is stopped cleanly
+            # except pika.exceptions.ConnectionClosedByBroker:
+            #     pass
+            continue
+        except pika.exceptions.StreamLostError:
+            continue
+        finally:
+            print("Restarted")
+            continue
